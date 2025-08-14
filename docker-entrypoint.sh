8689a5ec59acff243b994dc07c343523f4f67753
Junyeop Na <junyeop.na@navercorp.com>
2025-08-14 16:54:42 +0900

    set env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ if [ -n "$MODEL" ] && [ -z "$LLM_MODEL" ]; then
   export LLM_MODEL="$MODEL"
 fi
 
-echo $GEMINI_API_KEY
+export GITHUB_TOKEN="${INPUT_GITHUB_TOKEN:-${GITHUB_TOKEN}}"
+export GEMINI_API_KEY="${INPUT_GEMINI_API_KEY:-${GEMINI_API_KEY}}"
 
 exec python -m llm_pr_review_agent.pr_llm_review "$@" 
